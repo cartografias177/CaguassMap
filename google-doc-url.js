2c6c44b053238e5e1462aa3cb64f27aff1ee7333
cartografias177 <cartografias@lamarana.org>
2025-01-27 14:36:31 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/17HFgpsLXL3rFr1U_CJtsGhlXlMuYnNbMXoE7gUB9n_o/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTow2xQJkeLDSN4V26Y8hzdkw2ppDPyiOap8jOY25TjSZM7EisqHhcDGHHhzIA1waROxW0Q364GTh4B/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
